218f7c705695ecf7270e99b10d86cad971cb09d0
root <you@example.com>
2015-09-01 06:39:55 -0300

    Solucion header. -lg

--- client/app/scripts/controllers/main.js
+++ client/app/scripts/controllers/main.js
@@ -8,10 +8,15 @@
  * Controller of the slamApp
  */
 angular.module('slamApp')
-  .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+.controller('MainCtrl', function ($scope) {
+
+    $scope.setup_components = function() {
+        setTimeout(function() {
+            $("#home_slider_2").carousel({
+                interval:7e3
+            });
+        }, 1000);
+    };
+
+    $scope.setup_components();
+});
